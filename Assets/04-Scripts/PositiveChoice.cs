47440915271f4bfdd57e08060b9414337ba2b39d
SCU Imaginarium (Sub-Zero) <>
2023-06-01 14:00:40 -0700

    Press UI Button
    in the peptalk scene you can click the button, so hook that up to a counter next

---
+++ Assets/04-Scripts/PositiveChoice.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PositiveChoice : MonoBehaviour
+{
+    public int score;
+    public Text scoreText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	scoreText.text = score.ToString();    
+    }
+    
+    void Add()
+    {
+        score++;
+    }
+}
